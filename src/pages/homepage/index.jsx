8b0749280b12d5c0182b939bcf236c313548e53b
saksham-bharti <sakshambharti1805@gmail.com>
2025-10-09 01:01:04 +0530

    Fixed footer Logo

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -67,7 +67,7 @@ const Homepage = () => {
                 <div className="flex items-center space-x-3 mb-4">
                   <div className="w-10 h-10 bg-gradient-elevation rounded-lg flex items-center justify-center">
                     <img 
-                      src="/assets/images/DarkLogo.png" 
+                      src="/assets/images/Logo.png" 
                       alt="Description" 
                       className="w-full h-full object-cover rounded-lg"
                     />
